8a47fb1458a36ea2371a5b158b301392ba070711
aachips <aachips@gmail.com>
2023-10-19 15:31:24 -0400

    fixed almost everything except view

--- SAS/public/initialize-test.php
+++ SAS/public/initialize-test.php
@@ -1,6 +1,9 @@
 <?php include_once('../private/initialize.php');
 
-// echo "<p>Private path: " . PRIVATE_PATH . "</p>";
-// echo "<p>WWW_ROOT: " . WWW_ROOT . "</p>";
-
-// $
+echo "<p>Private Path: ".PRIVATE_PATH."</p>";
+echo "<p>Public Path: ".PUBLIC_PATH."</p>";
+echo "<p>Shared Path: ".SHARED_PATH."</p>";
+echo "<p>Project Path: ".PROJECT_PATH."</p>";
+echo "<p>Private Path: ".WWW_ROOT."</p>";
+echo $public_end;
+echo $doc_root;
