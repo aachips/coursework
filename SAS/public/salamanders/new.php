ff18396afc54cf0633677414b724488d76165797
aachips <aachips@gmail.com>
2023-10-26 01:03:29 -0400

    status unclear

--- SAS/public/salamanders/new.php
+++ SAS/public/salamanders/new.php
@@ -10,40 +10,38 @@
         echo 'No error'; 
         
     $page_title = 'Create Page';
-    include(SHARED_PATH . '/staff-header.php'); ?>
+    include(SHARED_PATH . '/salamander-header.php'); ?>
 
-<div id="content">
+    <div id="content">
 
-<a href="<?php echo url_for('/staff/pages/index.php'); ?>" class="back-link"></a>
-    <h1>Create Page</h1>
-    
-    <form action="<?= url_for('staff/Page/create.php');?>" method="post">
-    <dl>
-        <dt>Menu name</dt>
-        <dd><input type="text" name="menu_name" value=""></dd>
-    </dl>
-    <dl>
-        <dt>Position</dt>
-        <dd><select name="position" id="">
-            <option value="1">1</option>
-        </select>
-        </dd>
-    </dl>
-    <dl>
-        <dt>Visible</dt>
-        <dd>
-            <input type="hidden" name="visible" value="0">
-            <input type="checkbox" name="visible" value="1">
-        </dd>
-        <div id="operations">
-            <input type="submit" value="Create Page">
-        </div>
-    </dl>
-
-
-    </form>
+        <a href="<?php echo urlFor('../index.php'); ?>" class="back-link"></a>
+        <h1>Create Page</h1>
+        
+        <form action="<?= urlFor('create.php');?>" method="post">
+            <dl>
+                <dt>Menu name</dt>
+                <dd><input type="text" name="menu_name" value=""></dd>
+            </dl>
+            <dl>
+                <dt>Position</dt>
+                <dd><select name="position" id="">
+                    <option value="1">1</option>
+                </select>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Visible</dt>
+                <dd>
+                    <input type="hidden" name="visible" value="0">
+                    <input type="checkbox" name="visible" value="1">
+                </dd>
+                <div id="operations">
+                    <input type="submit" value="Create Page">
+                </div>
+            </dl>
+        </form>
 
 
-</div>    
+    </div>    
 
-<?php include(SHARED_PATH . '/salamander-footer.php'); ?>
+    <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
